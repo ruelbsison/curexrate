c2f3144dfc8c4a61e5fdedd8611c493f0b95aaea
Ruel Sison <ruelsison@Ruels-MacBook-Air.local>
2021-11-09 16:02:10 +0800

    Added curexrate-web-client

---
+++ curexrate-web-client/src/main/java/com/deca/gateway/curexrate/CurrencyExchangeRateWebClientApplication.java
@@ -0,0 +1,13 @@
+package com.deca.gateway.curexrate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyExchangeRateWebClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyExchangeRateWebClientApplication.class, args);
+	}
+
+}
---
+++ curexrate-web-client/src/test/java/com/deca/gateway/curexrate/CurrencyExchangeRateWebClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deca.gateway.curexrate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyExchangeRateWebClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
